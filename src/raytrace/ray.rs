df224e257d3c5d14792848f937ff8800ef847778
Thomas Sampson <tmsampson@gmail.com>
2019-05-27 15:55:19 +0100

    Added ray::RayHitResult

--- src/raytrace/ray.rs
+++ src/raytrace/ray.rs
@@ -3,14 +3,14 @@
 use super::vector::Vec3;
 
 // -----------------------------------------------------------------------------------------
-
+// Ray Type
 pub struct Ray {
     pub origin: Vec3,
     pub direction: Vec3,
 }
 
 // -----------------------------------------------------------------------------------------
-// Constructor
+// Ray Constructor
 impl Ray {
     pub fn new(origin: Vec3, direction: Vec3) -> Ray {
         Ray { origin, direction }
@@ -18,9 +18,52 @@ impl Ray {
 }
 
 // -----------------------------------------------------------------------------------------
-
+// Ray Members
 impl Ray {
     pub fn get_point(&self, t: f32) -> Vec3 {
         self.origin + (self.direction * t)
     }
 }
+
+// -----------------------------------------------------------------------------------------
+// RayHitResult Type
+#[derive(Copy, Clone)]
+pub struct RayHitResult {
+    pub hit: bool,
+    pub distance: f32,
+    pub position: Vec3,
+    pub normal: Vec3,
+}
+
+// -----------------------------------------------------------------------------------------
+// RayHitResult Constructor
+impl RayHitResult {
+    pub fn new(hit: bool, distance: f32, position: Vec3, normal: Vec3) -> RayHitResult {
+        RayHitResult {
+            hit,
+            distance,
+            position,
+            normal,
+        }
+    }
+}
+
+// -----------------------------------------------------------------------------------------
+// RayHitResult Constants
+impl RayHitResult {
+    pub const NO_HIT: RayHitResult = RayHitResult {
+        hit: false,
+        distance: 0.0,
+        position: Vec3::ZERO,
+        normal: Vec3::UP,
+    };
+
+    pub const MAX_HIT: RayHitResult = RayHitResult {
+        hit: false,
+        distance: std::f32::MAX,
+        position: Vec3::ZERO,
+        normal: Vec3::UP,
+    };
+}
+
+// -----------------------------------------------------------------------------------------
