719c8059951bae5177cff8ac3133c7e8be866255
Thomas Sampson <tmsampson@gmail.com>
2019-08-12 21:26:18 +0100

    Added window support (WIP)

--- src/main.rs
+++ src/main.rs
@@ -108,6 +108,12 @@ struct Job<'a> {
 // -----------------------------------------------------------------------------------------
 
 fn main() {
+    // info: https://lib.rs/crates/mini_gl_fb
+    // let mut fb = mini_gl_fb::gotta_go_fast("Hello world!", 800.0, 600.0);
+    // let buffer = vec![[128u8, 0, 0, 255]; 800 * 600];
+    // fb.update_buffer(&buffer);
+    // fb.persist();
+
     // Start timer
     let timer_begin = time::precise_time_s();
 
